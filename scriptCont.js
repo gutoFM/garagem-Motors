c378228a0853ed7680a0302eac64a701492c164e
gutoFM <gutofisco@gmail.com>
2023-05-13 19:20:53 -0300

    contact

--- scriptCont.js
+++ scriptCont.js
@@ -8,3 +8,16 @@ let mensagem = dia + " de " +meses[mes]+ " de " +ano
 
 let msg = document.getElementById('msg')
 msg.innerHTML = mensagem
+
+function validar(){
+    let nome = document.getElementById("nome").value
+    let telefone = document.getElementById("telefone").value
+    let email = document.getElementById("email").value
+
+    if(nome == "" && telefone == "" && email == ""){
+        alert("PREENCHA TODOS OS CAMPOS")
+    }else{
+        alert("ACESSO PERMITIDO")
+        window.open('enviado.html')
+    }
+}
